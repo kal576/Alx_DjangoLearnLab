7a180f56ddc869e4f72ad82140f2576a3b851b77
kal576 <cmuuo5864@gmail.com>
2025-07-10 16:13:25 +0300

    Fixed typo: ModelAdim to ModelAdmin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Book
 
-class BookAdmin(admin.ModelAdin):
+class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
 
 admin.site.register(Book, BookAdmin)
