2113cc72450bf123ba7a026ed4e965e92b16f193
kal576 <cmuuo5864@gmail.com>
2025-07-20 14:43:16 +0300

    added views and urls

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,21 +1,19 @@
 from django.shortcuts import render
-from .models import Book, Library
 from django.views.generic import DetailView
+from .models import Book, Library
 
-#function-based view to list all books with authors
+# Function-based view to display all books
 def list_books(request):
-    books = Books.objects.select_related('author').all()
-    output = "\n".join([f"{book.title} by {book.author.name}" for book in books])
-    return HttpResponse(output, content_type="text/plain")
+    books = Book.objects.all()  # Get all books from the database
+    return render(request, 'relationship_app/list_books.html', {'books': books})  # Render template with books
 
-#class-based view using DetailView
+# Class-based view to display details of a specific library
 class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+    model = Library  # Model for the view
+    template_name = 'relationship_app/library_detail.html'  # Template for rendering
+    context_object_name = 'library'  # Name used in template for library object
 
-    # Add books to context
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['books'] = self.object.books.all()
-        return context
+        context['books'] = self.object.books.all()  # Add all books in the library
+
