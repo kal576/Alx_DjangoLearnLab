31dc3ed7087b9fdc2010bbc1de67420d742fe544
kal576 <cmuuo5864@gmail.com>
2025-07-23 12:17:08 +0300

    Task 2: Preventing sql injection, xss attacks

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,23 +1,20 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required, permissions_required
-from .models import book
-from django import forms
+from django.contrib.auth.decorators import login_required, permission_required
+from .models import Book
+from .forms import ExampleForm
 
 @login_required
 @permissions_required('bookshelf.can_create', raise_exception=True)
 def add_book(request):
     books = Book.objects.all()
-    return render(request, 'bookshelf/book_list.html', {'books': books}))
-
-class SearchForm(forms.Form):
-    title = forms.CharField(max_length=10)
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
 #to prevent sql injection, use orms instead of raw sql queries
 def book_search(request):
-    form = SearchForm(request.GET or None)
-    book = []
+    form = ExampleForm(request.GET or None)
+    books = []
     if form.is_valid():
         title = form.cleaned_data['title']
         #secure orm query
         books = Book.objects.filter(title__icontains=title)
-    return render(rquest, 'bookshelf/book_list.html', {'form': form, 'books': books})
+    return render(request, 'bookshelf/book_list.html', {'form': form, 'books': books})
