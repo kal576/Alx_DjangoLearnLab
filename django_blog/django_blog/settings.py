d4ef66c6d0fd0196a1cfb572888bd7beb92464c1
kal576 <cmuuo5864@gmail.com>
2025-08-16 19:40:51 +0300

    Initial commit

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "blog",
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = "django_blog.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "blog",
+        "USER": "Caleb",
+        "PASSWORD": "C@Zajoel5864",
+        "HOST": "localhost",
+        "PORT": "5432"
     }
 }
 
@@ -115,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+        BASE_DIR / "static",
+        ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
