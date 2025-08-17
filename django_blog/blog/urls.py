5b6228b3f51d315ec8ec9fa434ef3bf81e9299ba
kal576 <cmuuo5864@gmail.com>
2025-08-17 19:34:26 +0300

    feat: add crud on posts

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,8 +7,11 @@ urlpatterns = [
         path("logout/", views.logout_view, name="logout"),
         path("register/", views.register_view, name="register"),
         path("profile/", views.profile_view, name="profile"),
-        path("posts/<int:post_id>/comments/new/", views.CommentCreateView.as_view(), name="comment-create"),
-        path("comments/<int:pk>/edit/", views.CommentUpdateView.as_view(), name="comment-update"),
-        `path("comments/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment-delete"),
+        path("post/", views.PostListView.as_view(), name="post-list"),
+        path("post/new/", views.PostCreateView.as_view(), name="post-create"),
+        path("post/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
+        path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name="post-update"),
+        path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post-delete"),ath("comments/<int:pk>/edit/", views.CommentUpdateView.as_view(), name="comment-update"),
+        path("comments/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment-delete"),
         ]
 
